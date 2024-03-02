983c5d4666f1e1b32d5b0bea3126bc9e9e579e61
Matias-DR <Matias-DR@outlook.com>
2024-03-02 04:08:19 -0300

    ADD: buying and selling for blue and official price task getters

--- dolariz_ar/dolariz_ar/tasksapp/tasks.py
+++ dolariz_ar/dolariz_ar/tasksapp/tasks.py
@@ -1,33 +1,64 @@
 import logging
 
 from bs4 import BeautifulSoup
-from constants import DOLLAR_SCRAPPER_URL as url
 from requests import get
+
+from constants import (
+    BLUE_DOLLAR_URL as blue_url,
+    OFFICIAL_DOLLAR_URL as official_url,
+)
 
 from .celery import app
 
 logger = logging.getLogger(__name__)
 
+
 @app.task
-def get_free() -> str:
+def get_blue() -> tuple[float, float]:
     """
-    Get the buy and sell values of the free dollar from dolarhoy.com
+    Get the official prices for buying and selling dollars on the web.
+
+    Returns:
+    - buying_price (float): blue dollar buy price.
+    - selling_price (float): blue dollar sell price.
     """
-    logger.warning("\nFREE DOLLAR CHECK EXECUTED")
-    soup = BeautifulSoup(response.content, "lxml")
-    buy, sell = (float(tag.string.removeprefix("$")) for tag in soup.select(".value"))
-    logger.warning(f"Buy: {buy} | Sell: {sell}\n")
-    return buy, sell
+
+    soup = (
+        BeautifulSoup(get(blue_url).content, "lxml")
+        .find("section", class_="modulo__cotizaciones")
+        .find("a", string="Dólar blue")
+        .parent
+    )
+    buying_price = (
+        soup.find("div", class_="compra")
+        .find("div", class_="val")
+        .string.removeprefix("$")
+    )
+    selling_price = (
+        soup.find("div", class_="venta")
+        .find("div", class_="val")
+        .string.removeprefix("$")
+    )
+
+    return buying_price, selling_price
 
 
-class DollarScrapper:
-    def __init__(self) -> None:
-        response = get(url)
-        self.soup = BeautifulSoup(response.content, "lxml")
-        buy, sell = (float(tag.string.removeprefix("$")) for tag in self.soup.select(".value"))
+@app.task
+def get_official() -> tuple[float, float]:
+    """
+    Get the official prices for buying and selling dollars on the web.
 
-    def get_free():
-        logger.warning("\nFREE DOLLAR CHECK EXECUTED")
+    Returns:
+    - buying_price (float): official dollar buy price.
+    - selling_price (float): official dollar sell price.
+    """
 
-    def get_official():
-        logger.warning("\nOFFICIAL DOLLAR CHECK EXECUTED")
+    soup = (
+        BeautifulSoup(get(official_url).content, "lxml")
+        .find("div", id="divisas")
+        .find(string="Dolar U.S.A")
+        .parent.parent.find_all("td")[1:]
+    )
+    buying_price = float(soup[0].string)
+    selling_price = float(soup[1].string)
+    return buying_price, selling_price
