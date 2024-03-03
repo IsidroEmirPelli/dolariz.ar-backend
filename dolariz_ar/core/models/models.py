6d28c1eb15d1064bf7e49821258d9682741dea1f
Matias-DR <Matias-DR@outlook.com>
2024-03-03 15:00:09 -0300

    ADD: docstring

--- dolariz_ar/core/models/models.py
+++ dolariz_ar/core/models/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db.models import CharField, DateTimeField, DecimalField, JSONField, Model
 from django.utils.crypto import get_random_string
 from django.utils.translation import gettext_lazy as _
 from django_enumfield import enum
@@ -7,27 +7,43 @@ from .enums import DollarType
 
 
 def generate_dollar_uuid():
+    """
+    Generate a random UUID for the uuid field of the dollar model.
+    """
+
     return "dollar_" + get_random_string(length=9)
 
 
-class Dollar(models.Model):
-    uuid = models.CharField(
+class Dollar(Model):
+    """
+    Dollar model to store the buying and selling dollar prices of any type of dollar in Argentina.
+
+    Attributes:
+    - uuid (CharField): Unique UUID for the dollar.
+    - price_buy (DecimalField): Price to buy the dollar.
+    - price_sell (DecimalField): Price to sell the dollar.
+    - date (DateTimeField): Date and time of the prices of the dollar.
+    - type_of_quote (EnumField): Type of dollar quote.
+    - extra_data (JSONField): Extra data for the dollar.
+    """
+
+    uuid = CharField(
         max_length=16,
         default=generate_dollar_uuid,
         primary_key=True,
-        help_text=_("Unique UUID for the dollar")
+        help_text=_("Unique UUID for the dollar"),
     )
-    price_buy = models.DecimalField(
+    price_buy = DecimalField(
         max_digits=10,
         decimal_places=2,
         help_text=_("Price to buy the dollar")
     )
-    price_sell = models.DecimalField(
+    price_sell = DecimalField(
         max_digits=10,
         decimal_places=2,
         help_text=_("Price to sell the dollar")
     )
-    date = models.DateTimeField(
+    date = DateTimeField(
         auto_now_add=True,
         help_text=_("Date and time of the dollar")
     )
@@ -35,7 +51,7 @@ class Dollar(models.Model):
         DollarType,
         help_text=_("Type of quote")
     )
-    extra_data = models.JSONField(
+    extra_data = JSONField(
         help_text=_("Extra data for the dollar"),
         default=dict,
     )
