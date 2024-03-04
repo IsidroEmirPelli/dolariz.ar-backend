50e95e523480249b17ddac248a77cd72dc6e1039
Matias-DR <Matias-DR@outlook.com>
2024-03-04 00:26:34 -0300

    ADD: serializer of cached dollar prices and variations

--- dolariz_ar/core/serializers.py
+++ dolariz_ar/core/serializers.py
@@ -1,4 +1,5 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, Serializer
+from django.db.models import DecimalField
 
 from .models import Dollar
 
@@ -8,8 +9,8 @@ class DollarSerializer(ModelSerializer):
     Serializer for the Dollar model.
     
     Attributes:
-    - price_buy (DecimalField): Price to buy the dollar.
-    - price_sell (DecimalField): Price to sell the dollar.
+    - price_buy (FloatField): Price to buy the dollar.
+    - price_sell (FloatField): Price to sell the dollar.
     - date (DateTimeField): Date and time of the prices of the dollar.
     - type_of_quote (EnumField): Type of dollar quote.
     - extra_data (JSONField): Extra data for the dollar.
@@ -24,3 +25,20 @@ class DollarSerializer(ModelSerializer):
             "type_of_quote",
             "extra_data",
         ]
+
+
+class CacheDollarSerializer(Serializer):
+    """
+    Serializer for the Dollar model to be used in the cache.
+    
+    Attributes:
+    - buying_price (DecimalField): Price to buy the dollar.
+    - selling_price (DecimalField): Price to sell the dollar.
+    - variation_buying_price (DecimalField): Variation of the buying price.
+    - variation_selling_price (DecimalField): Variation of the selling price.
+    """
+
+    buying_price = DecimalField()
+    selling_price = DecimalField()
+    variation_buying_price = DecimalField()
+    variation_selling_price = DecimalField()
