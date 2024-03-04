35fb206835b92caef0c77069da3972d970a978c6
Matias-DR <Matias-DR@outlook.com>
2024-03-04 00:51:49 -0300

    ADD: api for cached dollar prices and variations

--- dolariz_ar/api/views.py
+++ dolariz_ar/api/views.py
@@ -1,15 +1,18 @@
 import logging
 
-from core.models import Dollar
-from core.serializers import DollarSerializer
+from core.models import Dollar, DollarType
+from core.serializers import CacheDollarSerializer, DollarSerializer
+from core.services import get_official_dollar_prices_and_variations_from_cache_service
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.response import Response
 from rest_framework.status import (
     HTTP_200_OK,
     HTTP_400_BAD_REQUEST,
-    HTTP_404_NOT_FOUND,
+    # HTTP_404_NOT_FOUND,
 )
+
+from rest_framework.views import APIView
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from .filters import DollarFilterSet
@@ -28,6 +31,9 @@ class DollarViewSet(ReadOnlyModelViewSet):
     filterset_class = DollarFilterSet
     ordering_fields = ["price_buy", "price_sell", "date"]
 
+    # CONSULTAR lookup_field CON ISIDRO Y REFERENCIAR EL SERIALIZER
+    # lookup_field = "date"
+
     def list(self, request, *args, **kwargs):
         """
         List the dollar prices.
@@ -36,7 +42,7 @@ class DollarViewSet(ReadOnlyModelViewSet):
         try:
             queryset = self.filter_queryset(self.get_queryset())
             serializer = self.get_serializer(queryset, many=True)
-            logger.error(f"{self.__class__.__name__} list -> SUCCESS")
+            logger.info(f"{self.__class__.__name__} list -> SUCCESS")
             return Response(serializer.data, status=HTTP_200_OK)
         except Exception as e:
             logger.error(
@@ -55,7 +61,7 @@ class DollarViewSet(ReadOnlyModelViewSet):
         try:
             instance = self.get_object()
             serializer = self.get_serializer(instance)
-            logger.error(f"{self.__class__.__name__} retrieve -> SUCCESS")
+            logger.info(f"{self.__class__.__name__} retrieve -> SUCCESS")
             return Response(serializer.data, status=HTTP_200_OK)
         except Exception as e:
             logger.error(
@@ -63,5 +69,24 @@ class DollarViewSet(ReadOnlyModelViewSet):
             )
             return Response(
                 {"message": "Error al obtener el precio del dolar-"},
-                status=HTTP_404_NOT_FOUND,
+                status=HTTP_400_BAD_REQUEST,
             )
+
+
+class CachedDollarAPIView(APIView):
+    """
+    APIView for the cached dollar that allows to obtain their prices and variations.
+    """
+
+    def get(self, request, type_of_quote: DollarType) -> Response:
+        """
+        Get the cached dollar prices and variations.
+        """
+
+        data = get_official_dollar_prices_and_variations_from_cache_service(
+            type_of_quote
+        )
+        serializer = CacheDollarSerializer(data=data)
+        serializer.is_valid()
+        logger.info(f"{self.__class__.__name__} get -> SUCCESS")
+        return Response(serializer.data)
