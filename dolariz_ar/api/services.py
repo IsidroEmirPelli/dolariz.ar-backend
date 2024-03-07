53c9ea75a9bc2b68c2b6a8ddf8a1aed5edfff5b2
Matias-DR <Matias-DR@outlook.com>
2024-03-07 00:59:43 -0300

    ADD: api/service for get dollar from cache or db endpoint api

--- dolariz_ar/api/services.py
+++ dolariz_ar/api/services.py
@@ -1,7 +1,42 @@
-# Si por ejemplo, el método GET de X View, requiere procesar algún HTML para
-# scrapearlo, mas bien SOLO se encargará de realizar las tareas que hereda (las tareas
-# dispuestas por su padre, sease APIView, ModelViewSet, o etc.), de adaptar los
-# parámetros de entrada como por ejemplo extraer una query:
-# mi_query = (request.query.get('X_Query') or request.query.get('XQuery'))
-# , Y, instanciar al N servicio para que realice entonces el procesamiento del HTML,
-# pasándole el parámetro que contenga exe HTML. De esta forma 
+import logging
+
+from core.models import Dollar
+from core.services import (
+    get_official_dollar_prices_and_variations_from_cache_service,
+    calc_variation,
+)
+
+logger = logging.getLogger(__name__)
+
+
+def get_dollar_price_by_type_of_quote(type_of_quote: str) -> dict:
+    data = None
+    try:
+        data = get_official_dollar_prices_and_variations_from_cache_service(
+            type_of_quote
+        )
+        if not data:
+            prices = (
+                Dollar.objects
+                .filter(type_of_quote=type_of_quote)
+                .latest("date")[:2]
+            )
+            variation_buying_price = calc_variation(
+                prices[1].price_buy,
+                prices[0].price_buy
+            ),
+            variation_selling_price = calc_variation(
+                prices[1].price_sell,
+                prices[0].price_sell
+            )
+            data = {
+                "buying_price": prices[0].price_buy,
+                "selling_price": prices[0].price_sell,
+                "variation_buying_price": variation_buying_price,
+                "variation_selling_price": variation_selling_price,
+            }
+    except Exception as e:
+        logger.error(
+            f"get_dollar_price_by_type_of_quote obtain_price -> Error obtaining the dollar price: {e}"
+        )
+    return data
