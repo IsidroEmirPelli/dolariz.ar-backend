c5f411f967e172b0a07736e80a1991e8c88cb215
Matias-DR <Matias-DR@outlook.com>
2024-03-13 01:16:24 -0300

    ADD: type_of_quote filter field

--- dolariz_ar/api/filters.py
+++ dolariz_ar/api/filters.py
@@ -1,5 +1,9 @@
 from core.models import Dollar
-from django_filters.rest_framework import DateTimeFilter, FilterSet
+from django_filters.rest_framework import (
+    DateTimeFilter,
+    FilterSet,
+    NumberFilter
+)
 
 
 class DollarFilterSet(FilterSet):
@@ -7,10 +11,15 @@ class DollarFilterSet(FilterSet):
     FilterSet for the Dollar model.
 
     Attributes:
+    - type_of_quote (str): Dollar quotation.
     - date_from (DateTimeFilter): "From" date and time of the dollar.
     - date_to (DateTimeFilter): "To" date and time of the dollar.
     """
 
+    type_of_quote = NumberFilter(
+        field_name="type_of_quote",
+        help_text="Dollar quotation",
+    )
     date_from = DateTimeFilter(
         field_name="date",
         help_text='"From" date and time of the dollar',
