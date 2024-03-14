163ea72bde397552c0fb7b1e4bdf3f1cfbdc4374
IsidroPelli <isidroemirpelli@gmail.com>
2024-03-13 23:08:44 -0300

    Actualiza el conector LEDES DollarRecruiter para utilizar DollarRecuiterByma y obtener precios de compra y venta de MEP dollar desde la web

--- dolariz_ar/core/connectors/ledes_dollar_recuiter.py
+++ dolariz_ar/core/connectors/ledes_dollar_recuiter.py
@@ -1,17 +1,43 @@
 import logging
 
 from core.models import DollarType
-
-from core.connectors.dollar_recuiter import DollarRecruiter
+from core.connectors.dollar_recuiter_byma import DollarRecuiterByma
+from constants import LEDES_DOLLAR_URL
 
 logger = logging.getLogger(__name__)
 
 
-class LEDESDollarRecruiter(DollarRecruiter):
+class LEDESDollarRecruiter(DollarRecuiterByma):
     def __init__(self) -> None:
         """
-        Calculates the buying and selling prices for the LEDES dollar from the web.
+        Calculates the buying and selling prices for the MEP dollar from the web COnvert this request into a method
         """
 
+        self.url = LEDES_DOLLAR_URL
+        self.get_type_of_quote = DollarType.LEDES
+        self.bond_value = "X20Y4"
+        self.bondD_value = "XY4D"
+        self.data = {
+            "excludeZeroPxAndQty": True,
+            "T2": True,
+            "T1": False,
+            "T0": False,
+            "Content-Type": "application/json",
+        }
+
         super().__init__()
-        self.type_of_quote = DollarType.LEDES.value
+
+    def get_prices_from_response(self) -> tuple[float]:
+        """
+        Get the buying and selling prices from the response of the web.
+
+        Returns
+        -------
+        tuple[float]
+            The buying and selling prices.
+        """
+
+        bond_buying_price = self.bond[0].get("vwap", None)
+        bondD_selling_price = self.bondD[0].get("vwap", None)
+
+        return bond_buying_price, bondD_selling_price
